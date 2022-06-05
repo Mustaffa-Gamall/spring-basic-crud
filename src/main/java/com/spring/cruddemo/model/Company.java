4bf34b99a4e8921fc4d59de68fa43cc6e379161d
Mustafa_gamal <mustaffa.gamall@outlook.com>
2022-06-05 11:54:07 +0200

    modify file

--- src/main/java/com/spring/cruddemo/model/Company.java
+++ src/main/java/com/spring/cruddemo/model/Company.java
@@ -2,6 +2,7 @@ package com.spring.cruddemo.model;
 
 import java.util.Set;
 import javax.persistence.Entity;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
